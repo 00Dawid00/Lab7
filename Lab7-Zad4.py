2c008998385d4ae56a806330a14a4fe2b0e530d4
w72420 <d.stanisz00@gmail.com>
2024-11-26 11:20:07 +0100

    Lab7

--- Lab7-Zad4.py
+++ Lab7-Zad4.py
@@ -19,4 +19,37 @@ pracownicy_pensja_over_5000 = df[df['Pensja'] > 5000]
 print("\nPracownicy z pensją większą niż 5000 PLN:")
 print(pracownicy_pensja_over_5000)
 
+#B
+pracownicy_posortowani_wiek = df.sort_values(by='Wiek')
+print("\nPracownicy posortowani według wieku:")
+print(pracownicy_posortowani_wiek)
 
+#C
+srednia_pensja_stanowisko = df.groupby('Stanowisko')['Pensja'].mean()
+print("\nŚrednia pensja w zależności od stanowiska:")
+print(srednia_pensja_stanowisko)
+
+#D
+awans_data = {
+    'Numer ID': [1, 4],
+    'Imię': ['Anna', 'Tomasz'],
+    'Nazwisko': ['Kowalska', 'Kaczmarek'],
+    'Stanowisko': ['Dyrektor', 'Konsultant Senior'],
+    'Wiek': [35, 30],
+    'Pensja': [10000, 8000]
+}
+
+df_awans = pd.DataFrame(awans_data)
+
+df_polaczone = pd.merge(df, df_awans, on='Numer ID', how='outer', suffixes=('_przed', '_po'))
+print("\nPracownicy przed i po awansie:")
+print(df_polaczone)
+
+#E
+df.to_csv('pracownicy.csv', index=False)
+
+#F
+df_wczytane = pd.read_csv('pracownicy.csv')
+
+print("\nWczytane dane z pliku CSV:")
+print(df_wczytane)
